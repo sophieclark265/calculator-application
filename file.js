244ff46dd6ece482d6da2584ad51293215b11368
Sophie <sophieclark526@gmail.com>
2022-07-05 00:01:01 +0200

    add js file. create basic math functions in js file.

---
+++ file.js
@@ -0,0 +1,25 @@
+console.log("hey there");
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(3, 4)); // should get 7
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(3, 3)); // should get 9
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(divide(10, 5)); // should get 2
+
+function minus(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(minus(2, 1)); // should get 1
