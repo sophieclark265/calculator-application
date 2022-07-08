d0697f50267e8e5bf465128331ea5e3ceee8508a
Sophie <sophieclark526@gmail.com>
2022-07-08 17:37:07 +0200

    split up functions into smaller functions, populate top screen with workings and deliver result in bottom screen

--- file.js
+++ file.js
@@ -1,23 +1,17 @@
 let digits = document.querySelectorAll(".digits");
-
-let responsiveDigits = digits.forEach((digit) => {
-  digit.addEventListener("click", populateBottomScreen);
-});
-
-let holdAnswer = 0;
-
-let bottomScreen = document.querySelector(".bottom-screen"); // grab bigScreen HTML dom
-let topScreen = document.querySelector(".top-screen");
-let operandChosen;
-let firstInput = topScreen.textContent;
-let secondInput = bottomScreen.textContent;
-
 let equals = document.querySelector(".digits-equal");
 let decimal = document.querySelector("#decimal");
+let bottomScreen = document.querySelector(".bottom-screen"); // grab bigScreen HTML dom
+let topScreen = document.querySelector(".top-screen");
+
+let responsiveDigits = digits.forEach((digit) => {
+  digit.addEventListener("click", handleDigitClick);
+});
 decimal.addEventListener("click", onClickDecimal);
+equals.addEventListener("click", getFinalResult);
 
-equals.addEventListener("click", getAnswer);
-
+let holdAnswer = 0;
+let operandChosen;
 let arrayOfInputs = [];
 
 // operand functions
@@ -51,7 +45,7 @@ function minus(num1, num2) {
 
 //
 
-function populateBottomScreen(event) {
+function handleDigitClick(event) {
   // fill out bottom screen
   let digitChosen = event.target.innerText; // gets number or operand chosen
   if (
@@ -61,50 +55,38 @@ function populateBottomScreen(event) {
     digitChosen == "+" ||
     digitChosen == "C"
   ) {
+    crunchNumbers();
     clearBottomScreen(digitChosen); // clear bottom screen
   } else {
-    bottomScreen.textContent += digitChosen; // populate bottom screen with digit chosen
-    arrayOfInputs.push(digitChosen);
-    console.log("first function array of Inputs state " + arrayOfInputs);
-    if (arrayOfInputs[0] && arrayOfInputs[1]) {
-      let num1 = arrayOfInputs[0];
-      let num2 = arrayOfInputs[1];
-      executePair(num1, num2);
-    }
+    saveBottomScreenInput(digitChosen);
   }
 }
 
-function executePair(num1, num2) {
-  console.log("running execute pair func");
-  let answer;
-  if (operandChosen == "*") {
-    answer = multiply(num1, num2);
-  } else if (operandChosen == "/") {
-    answer = divide(num1, num2);
-  } else if (operandChosen == "+") {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    answer = add(num1, num2);
-  } else if (operandChosen == "-") {
-    answer = minus(num1, num2);
+function saveBottomScreenInput(input) {
+  bottomScreen.textContent += input; // populate bottom screen with digit chosen
+  arrayOfInputs.push(input);
+}
+
+function crunchNumbers() {
+  if (!arrayOfInputs[0] || !arrayOfInputs[1]) {
+    return;
   }
-
+  let num1 = arrayOfInputs[0];
+  let num2 = arrayOfInputs[1];
+  console.log("running execute pair func");
+  let answer = getAnswer(num1, num2);
   let numberAnswer = Number(answer);
+  saveComputation(numberAnswer);
+}
 
-  holdAnswer = numberAnswer;
-
-  if (isFinite(holdAnswer)) {
+function saveComputation(savedAnswer) {
+  if (isFinite(savedAnswer)) {
     // only if finite number
-    topScreen.textContent = holdAnswer;
-    bottomScreen.textContent = "";
     arrayOfInputs = []; // 1 + 1 is 2, delete array and push 2
-    operandChosen = 0; // then if i add 1 again, i need to get 3
-    arrayOfInputs.push(holdAnswer);
+    arrayOfInputs.push(savedAnswer);
   } else {
     // if holdAnswer is infinite, call error func
     topScreen.textContent = "";
-    arrayOfInputs.splice(-1, 1);
-    console.log("just spliced array of Inputs " + arrayOfInputs);
     errorMessage();
   }
 }
@@ -119,7 +101,6 @@ function clearBottomScreen(digitChosen) {
     arrayOfInputs = [];
   } else {
     operandChosen = digitChosen;
-    // fill out top screen, then reset bottom screen
     let bottomScreenText = bottomScreen.textContent;
     populateTopScreen(bottomScreenText, operandChosen);
     bottomScreen.textContent = ""; // reset bottom screen
@@ -127,21 +108,25 @@ function clearBottomScreen(digitChosen) {
 }
 
 function populateTopScreen(bottomScreenText, operandChosen) {
-  topScreen.textContent = bottomScreenText + operandChosen; // give top screen text that was in bottom screen
+  topScreen.textContent += bottomScreenText + operandChosen; // give top screen text that was in bottom screen
   console.log("this is array so far " + arrayOfInputs);
 }
 
-// function operandDecider(num1, num2, operand) {
-//   console.log("operand decider func has been called");
-// }
-// event object
-// target attribute or field
+function getFinalResult() {
+  if (arrayOfInputs.length == 1) {
+    let result = arrayOfInputs[0]; // if only one element in array before equals pressed, then first element is answer bc crunchNumbers has calculated result
+    bottomScreen.textContent = result;
+  } else {
+    let num1 = arrayOfInputs[0];
+    let num2 = arrayOfInputs[1];
+    let result = getAnswer(num1, num2);
+    bottomScreen.textContent = result;
+  }
+}
 
-function getAnswer() {
+function getAnswer(num1, num2) {
   // operand top number by bottom number
   let answer;
-  let num2 = bottomScreen.textContent;
-  let num1 = topScreen.textContent;
   if (operandChosen == "*") {
     answer = multiply(num1, num2);
   } else if (operandChosen == "/") {
@@ -153,9 +138,7 @@ function getAnswer() {
   } else if (operandChosen == "-") {
     answer = minus(num1, num2);
   }
-  topScreen.textContent = "";
-  bottomScreen.textContent = "";
-  bottomScreen.textContent = answer;
+  return answer;
 }
 
 function onClickDecimal(event) {
@@ -167,11 +150,8 @@ function onClickDecimal(event) {
 }
 
 function errorMessage() {
-  console.log("running error message function");
   return (bottomScreen.textContent = "Oops! Can't divide by 0.");
 }
-
-let arraySpliced = false;
 
 // as soon as two inputs with one operand, need to do calculation and store result
 //
